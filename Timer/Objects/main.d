7a32ebb418f4c61ead476b2a5e17c84a41521460
malkh <mohammadalkhaledi2@gmail.com>
2021-12-04 14:48:42 -0500

    changed timer control config to change individual bits rather than set control equal to 0x0122. changed ta0_n isr to clear ifg at start of routine

--- Timer/Objects/main.d
+++ Timer/Objects/main.d
@@ -7,35 +7,4 @@
   C:\Users\Mohammad\AppData\Local\Arm\Packs\ARM\CMSIS\5.8.0\CMSIS\Core\Include\cmsis_compiler.h \
   C:\Users\Mohammad\AppData\Local\Arm\Packs\ARM\CMSIS\5.8.0\CMSIS\Core\Include\cmsis_armclang.h \
   C:\Users\Mohammad\AppData\Local\Arm\Packs\ARM\CMSIS\5.8.0\CMSIS\Core\Include\mpu_armv7.h \
-  C:\Users\Mohammad\AppData\Local\Arm\Packs\TexasInstruments\MSP432P4xx_DFP\3.2.6\Device\Include\system_msp432p4111.h \
-  C:\Users\Mohammad\OneDrive\ -\ Carleton\ University\Desktop\code\ +\ school\YEAR\ 3\SYSC3310\MSP432P4xx\driverlib.h \
-  C:\Users\Mohammad\OneDrive\ -\ Carleton\ University\Desktop\code\ +\ school\YEAR\ 3\SYSC3310\MSP432P4xx\adc14.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdbool.h \
-  C:\Users\Mohammad\OneDrive\ -\ Carleton\ University\Desktop\code\ +\ school\YEAR\ 3\SYSC3310\MSP432P4xx\aes256.h \
-  C:\Users\Mohammad\OneDrive\ -\ Carleton\ University\Desktop\code\ +\ school\YEAR\ 3\SYSC3310\MSP432P4xx\comp_e.h \
-  C:\Users\Mohammad\OneDrive\ -\ Carleton\ University\Desktop\code\ +\ school\YEAR\ 3\SYSC3310\MSP432P4xx\cpu.h \
-  C:\Users\Mohammad\OneDrive\ -\ Carleton\ University\Desktop\code\ +\ school\YEAR\ 3\SYSC3310\MSP432P4xx\crc32.h \
-  C:\Users\Mohammad\OneDrive\ -\ Carleton\ University\Desktop\code\ +\ school\YEAR\ 3\SYSC3310\MSP432P4xx\cs.h \
-  C:\Users\Mohammad\OneDrive\ -\ Carleton\ University\Desktop\code\ +\ school\YEAR\ 3\SYSC3310\MSP432P4xx\dma.h \
-  C:\Users\Mohammad\OneDrive\ -\ Carleton\ University\Desktop\code\ +\ school\YEAR\ 3\SYSC3310\MSP432P4xx\eusci.h \
-  C:\Users\Mohammad\OneDrive\ -\ Carleton\ University\Desktop\code\ +\ school\YEAR\ 3\SYSC3310\MSP432P4xx\flash.h \
-  C:\Users\Mohammad\OneDrive\ -\ Carleton\ University\Desktop\code\ +\ school\YEAR\ 3\SYSC3310\MSP432P4xx\fpu.h \
-  C:\Users\Mohammad\OneDrive\ -\ Carleton\ University\Desktop\code\ +\ school\YEAR\ 3\SYSC3310\MSP432P4xx\gpio.h \
-  C:\Users\Mohammad\OneDrive\ -\ Carleton\ University\Desktop\code\ +\ school\YEAR\ 3\SYSC3310\MSP432P4xx\i2c.h \
-  C:\Users\Mohammad\OneDrive\ -\ Carleton\ University\Desktop\code\ +\ school\YEAR\ 3\SYSC3310\MSP432P4xx\interrupt.h \
-  C:\Users\Mohammad\OneDrive\ -\ Carleton\ University\Desktop\code\ +\ school\YEAR\ 3\SYSC3310\MSP432P4xx\mpu.h \
-  C:\Users\Mohammad\OneDrive\ -\ Carleton\ University\Desktop\code\ +\ school\YEAR\ 3\SYSC3310\MSP432P4xx\pcm.h \
-  C:\Users\Mohammad\OneDrive\ -\ Carleton\ University\Desktop\code\ +\ school\YEAR\ 3\SYSC3310\MSP432P4xx\pmap.h \
-  C:\Users\Mohammad\OneDrive\ -\ Carleton\ University\Desktop\code\ +\ school\YEAR\ 3\SYSC3310\MSP432P4xx\pss.h \
-  C:\Users\Mohammad\OneDrive\ -\ Carleton\ University\Desktop\code\ +\ school\YEAR\ 3\SYSC3310\MSP432P4xx\ref_a.h \
-  C:\Users\Mohammad\OneDrive\ -\ Carleton\ University\Desktop\code\ +\ school\YEAR\ 3\SYSC3310\MSP432P4xx\reset.h \
-  C:\Users\Mohammad\OneDrive\ -\ Carleton\ University\Desktop\code\ +\ school\YEAR\ 3\SYSC3310\MSP432P4xx\rom.h \
-  C:\Users\Mohammad\OneDrive\ -\ Carleton\ University\Desktop\code\ +\ school\YEAR\ 3\SYSC3310\MSP432P4xx\rom_map.h \
-  C:\Users\Mohammad\OneDrive\ -\ Carleton\ University\Desktop\code\ +\ school\YEAR\ 3\SYSC3310\MSP432P4xx\rtc_c.h \
-  C:\Users\Mohammad\OneDrive\ -\ Carleton\ University\Desktop\code\ +\ school\YEAR\ 3\SYSC3310\MSP432P4xx\spi.h \
-  C:\Users\Mohammad\OneDrive\ -\ Carleton\ University\Desktop\code\ +\ school\YEAR\ 3\SYSC3310\MSP432P4xx\sysctl.h \
-  C:\Users\Mohammad\OneDrive\ -\ Carleton\ University\Desktop\code\ +\ school\YEAR\ 3\SYSC3310\MSP432P4xx\systick.h \
-  C:\Users\Mohammad\OneDrive\ -\ Carleton\ University\Desktop\code\ +\ school\YEAR\ 3\SYSC3310\MSP432P4xx\timer32.h \
-  C:\Users\Mohammad\OneDrive\ -\ Carleton\ University\Desktop\code\ +\ school\YEAR\ 3\SYSC3310\MSP432P4xx\timer_a.h \
-  C:\Users\Mohammad\OneDrive\ -\ Carleton\ University\Desktop\code\ +\ school\YEAR\ 3\SYSC3310\MSP432P4xx\uart.h \
-  C:\Users\Mohammad\OneDrive\ -\ Carleton\ University\Desktop\code\ +\ school\YEAR\ 3\SYSC3310\MSP432P4xx\wdt_a.h
+  C:\Users\Mohammad\AppData\Local\Arm\Packs\TexasInstruments\MSP432P4xx_DFP\3.2.6\Device\Include\system_msp432p4111.h
